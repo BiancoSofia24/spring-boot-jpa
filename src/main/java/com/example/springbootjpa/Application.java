cb0fa8a2707501db22959acff5b5d2e47f1161e8
BiancoSofia24 <b.sofia2124@gmail.com>
2021-05-25 20:00:25 -0300

    jpa practice finished

--- src/main/java/com/example/springbootjpa/Application.java
+++ src/main/java/com/example/springbootjpa/Application.java
@@ -13,6 +13,8 @@ import com.example.springbootjpa.repository.StudentIdCardRepository;
 import com.example.springbootjpa.repository.StudentRepository;
 import com.github.javafaker.Faker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,6 +23,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -28,8 +32,10 @@ public class Application {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository repository, StudentIdCardRepository cardRepository) {
 		return args -> {
+
             Faker faker = new Faker();
 
+			log.info("Creating student");
             String firstName = faker.name().firstName();
             String lastName = faker.name().lastName();
             String email = String.format("%s.%s@mail.com", firstName, lastName);
@@ -39,53 +45,52 @@ public class Application {
                     email,
                     faker.number().numberBetween(17, 55));
 
-            student.addBook(
-                    new Book(LocalDateTime.now().minusDays(4), "Clean Code"));
+			log.info("Adding books to the created student");
+            student.addBook(new Book(LocalDateTime.now().minusDays(4), "Clean Code"));
 
-            student.addBook(
-                    new Book(LocalDateTime.now(), "Clean Architecture"));
+            student.addBook(new Book(LocalDateTime.now(), "Clean Architecture"));
 
-            student.addBook(
-                    new Book(LocalDateTime.now().minusYears(1), "Spring Data JPA"));
+            student.addBook(new Book(LocalDateTime.now().minusYears(1), "Spring Data JPA"));
 
+			log.info("Adding an Id Card to the created student");
             StudentIdCard studentIdCard = new StudentIdCard(
                     "123456789",
                     student);
-
             student.setStudentIdCard(studentIdCard);
 
+			log.info("Enrolling the student to a few courses");
             student.addEnrolment(new Enrolment(
-                    new EnrolmentId(1L, 1L),
-                    student,
-                    new Course("React", "IT"),
-                    LocalDateTime.now()
+                new EnrolmentId(1L, 1L),
+                student,
+                new Course("React", "IT"),
+                LocalDateTime.now()
             ));
 
             student.addEnrolment(new Enrolment(
-                    new EnrolmentId(1L, 2L),
-                    student,
-                    new Course("Spring Boot", "IT"),
-                    LocalDateTime.now().minusDays(18)
+                new EnrolmentId(1L, 2L),
+                student,
+                new Course("Spring Boot", "IT"),
+                LocalDateTime.now().minusDays(18)
             ));
 
             student.addEnrolment(new Enrolment(
-                    new EnrolmentId(1L, 2L),
-                    student,
-                    new Course("Spring Boot", "IT"),
-                    LocalDateTime.now().minusDays(18)
+                new EnrolmentId(1L, 2L),
+                student,
+                new Course("Spring Boot", "IT"),
+                LocalDateTime.now().minusDays(18)
             ));
 
+			log.info("Save the student into the db");
             repository.save(student);
 
             repository.findById(1L)
-                    .ifPresent(s -> {
-                        System.out.println("Fetch books lazy...");
-                        List<Book> books = student.getBooks();
-                        books.forEach(book -> {
-                            System.out.println(
-                                    s.getFirstName() + " borrowed " + book.getBookName());
-                        });
+                .ifPresent(s -> {
+                    log.info("Fetch books lazy...");
+                    List<Book> books = student.getBooks();
+                    books.forEach((book) -> {
+                        log.info(s.getFirstName() + " borrowed " + book.getBookName());
                     });
+                });
 
         };
 	}
--- src/main/java/com/example/springbootjpa/repository/StudentRepository.java
+++ src/main/java/com/example/springbootjpa/repository/StudentRepository.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
     
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
